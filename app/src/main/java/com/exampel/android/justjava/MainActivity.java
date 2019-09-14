8b9f944f003d0aab8cf1470e6a173c9fdbfab587
italiy <vit47903@gmail.com>
2019-09-14 23:02:44 +0300

    project was created

---
+++ app/src/main/java/com/exampel/android/justjava/MainActivity.java
@@ -0,0 +1,33 @@
+package com.exampel.android.justjava;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        display(1);
+        displayPrice(2 * 5);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+
+}
