87ce90f6601dac2cc7cd8bd490942f5ccd798a96
italiy <vit47903@gmail.com>
2019-09-15 22:46:56 +0300

    added buttons "+", "-".

--- app/src/main/java/com/exampel/android/justjava/MainActivity.java
+++ app/src/main/java/com/exampel/android/justjava/MainActivity.java
@@ -7,7 +7,10 @@ import android.widget.TextView;
 
 import java.text.NumberFormat;
 
+import static com.exampel.android.justjava.R.id.prise_text_view;
+
 public class MainActivity extends Activity {
+    protected int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +19,35 @@ public class MainActivity extends Activity {
     }
 
     public void submitOrder(View view) {
-        display(1);
-        displayPrice(2 * 5);
+
+        display(quantity);
+        displayPrice(quantity * 5);
     }
+
+    public void increment(View view) {
+
+        quantity++;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+
+        if (quantity <= 0) {
+            quantity = quantity;
+        }else{
+            quantity--;
+        }
+        display(quantity);
+    }
+
 
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(prise_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
-
-
 }
